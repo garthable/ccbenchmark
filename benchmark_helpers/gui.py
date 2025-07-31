3b1081854e58debc5c2daf07ba6a6835d6e93907
Dalton Prokosch <ddjjpp331@gmail.com>
2025-07-31 15:40:33 -0600

    moved tree gui to gui

--- benchmark_helpers/gui.py
+++ benchmark_helpers/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
 import tkinter.font as tkFont
 import logging
 import sys
@@ -10,11 +10,49 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 try:
-    from benchmark_helpers.tree_gui import create_hierarchy
     from benchmark_helpers.benchmark_data import BenchmarkData, BenchmarkColumn, TimeType
 except ImportError as e:
     logger.critical(f"Failed to import benchmark_data: {e}")
     sys.exit(IMPORT_FAILURE_EXIT_CODE)
+
+def create_hierarchy(benchmark_data: BenchmarkData, parent: ttk.Frame) -> ttk.Treeview:
+    created_paths = set()
+
+    hierarchy = ttk.Treeview(parent)
+
+    hierarchy.column("#0", minwidth=100, width=500, stretch=True)
+    hierarchy.heading('#0', text='Benchmarks', anchor='w')
+
+    for i, (col, benchmark_name) in enumerate(zip(benchmark_data.matrix, benchmark_data.benchmark_names)):
+        prev = ''
+        for part in col.benchmark_bin_path.parts:
+            segment = f'{prev}/{part}'
+            if segment not in created_paths:
+                hierarchy.insert(str(prev), len(created_paths), str(segment), text=part)
+                created_paths.add(segment)
+            prev = segment
+        segment = f'{prev}/{benchmark_name}'
+        if segment not in created_paths:
+            hierarchy.insert(str(prev), len(created_paths), str(segment), text=benchmark_name, values=(i))
+            created_paths.add(segment)
+
+    hierarchy.update_idletasks()
+    hierarchy.event_generate("<Configure>")
+
+    hierarchy.grid(row=0, column=1, sticky='nsew')
+
+    v_scrollbar = ttk.Scrollbar(parent, command=hierarchy.yview, orient='vertical')
+    v_scrollbar.grid(row=0, column=0, sticky='ns')
+
+    h_scrollbar = ttk.Scrollbar(parent, command=hierarchy.xview, orient='horizontal')
+    h_scrollbar.grid(row=1, column=1, sticky='ew')
+
+    hierarchy.configure(yscrollcommand=v_scrollbar.set, xscrollcommand=h_scrollbar.set)
+
+    parent.columnconfigure(1, weight=1)
+    parent.rowconfigure(0, weight=1)
+
+    return hierarchy
 
 def get_columns(is_aggregated: bool) -> list[str]:
     if not is_aggregated:
@@ -23,6 +61,11 @@ def get_columns(is_aggregated: bool) -> list[str]:
              'Median Time', 'Median Time Delta (%)', 'Standard Deviation', 
              'Standard Deviation Delta (%)', 'Coefficient of Variation (%)', 
              'Coefficient of Variation Delta (%)']
+
+def darken(hex_color, amount=10):
+    hex_color = hex_color.lstrip("#")
+    rgb = [max(0, int(hex_color[i:i+2], 16) - amount) for i in (0, 2, 4)]
+    return "#{:02x}{:02x}{:02x}".format(*rgb)
 
 def create_benchmark_table(parent, benchmark_data: BenchmarkData, is_aggregated: bool):
     """Generates data table"""
@@ -33,9 +76,13 @@ def create_benchmark_table(parent, benchmark_data: BenchmarkData, is_aggregated:
     tree['show'] = 'headings'
 
     font = tkFont.Font()
+
+    bg = style.lookup("Treeview", "background")
+    even_bg = darken(bg, amount=10)
+    odd_bg = darken(bg, amount=5)
     
-    tree.tag_configure('evenrow', background='#f0f0ff')
-    tree.tag_configure('oddrow', background='white')
+    tree.tag_configure('evenrow', background=even_bg)
+    tree.tag_configure('oddrow', background=odd_bg)
     for i in range(len(benchmark_data.iteration_names)):
         tag = 'evenrow' if i % 2 == 0 else 'oddrow'
         tree.insert('', 'end', values=['' for i in range(len(columns))], tags=(tag,))
@@ -47,7 +94,7 @@ def create_benchmark_table(parent, benchmark_data: BenchmarkData, is_aggregated:
             text = tree.set(item, col)
             max_width = max(max_width, font.measure(text))
         max_width += 20
-        tree.column(col, minwidth=max_width, width=max_width, stretch=False, anchor='e')
+        tree.column(col, minwidth=max_width, width=max_width, stretch=True, anchor='e')
 
     horizontal_scrollbar = ttk.Scrollbar(parent, command=tree.xview, orient='horizontal')
     horizontal_scrollbar.grid(row=1, column=0, sticky='ew')
@@ -62,9 +109,12 @@ def create_benchmark_table(parent, benchmark_data: BenchmarkData, is_aggregated:
 
 def show_gui(benchmark_data: BenchmarkData):
     """Displays gui for benchmark results"""
-    root = tk.Tk()
+    root = ttk.Window(themename='solar')
     root.minsize(600, 400)
     root.title("Benchmark Results")
+
+    global style
+    style = ttk.Style()
 
     horizontal_split = ttk.PanedWindow(root, orient='horizontal')
     horizontal_split.pack(fill='both', expand=True)
@@ -77,7 +127,6 @@ def show_gui(benchmark_data: BenchmarkData):
 
     right_frame.rowconfigure(0, weight=1)
     right_frame.columnconfigure(0, weight=1)
-
         
     hierarchy = create_hierarchy(benchmark_data, left_frame)
     table = create_benchmark_table(right_frame, benchmark_data, True)
