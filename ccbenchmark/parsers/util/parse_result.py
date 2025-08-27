bd30eb61836807119620a3292be3cb24d47994e5
Dalton Prokosch <ddjjpp331@gmail.com>
2025-08-27 08:08:13 -0600

    resolved circular import

--- ccbenchmark/parsers/util/parse_result.py
+++ ccbenchmark/parsers/util/parse_result.py
@@ -1,10 +1,11 @@
-from ccbenchmark.benchmark_data import BenchmarkTime
+from __future__ import annotations
+import ccbenchmark.benchmark_data as bd
 from dataclasses import dataclass
 
 @dataclass(slots=True)
 class ParseResult:
-    real_time: BenchmarkTime
-    cpu_time: BenchmarkTime
+    real_time: bd.BenchmarkTime
+    cpu_time: bd.BenchmarkTime
 
     benchmark_index: int
     metric_index: int
