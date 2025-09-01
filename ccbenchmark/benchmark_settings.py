8f43a5da9061482662d80bb40617d6eb3511fbd4
Dalton Prokosch <ddjjpp331@gmail.com>
2025-08-31 18:39:03 -0600

    renamed path to be all caps, cleaned up the logic of load

--- ccbenchmark/benchmark_settings.py
+++ ccbenchmark/benchmark_settings.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from pathlib import Path
 import yaml
 
-_local_settings_file = Path('./.ccbenchmark/settings.yaml')
+_LOCAL_SETTINGS_FILE = Path('./.ccbenchmark/settings.yaml')
 
 @dataclass
 class LocalSettings:
@@ -12,22 +12,20 @@ class LocalSettings:
     output_dir: Path
 
 def save_local_settings(settings: LocalSettings):
-    with open(_local_settings_file, 'x') as file:
+    with open(_LOCAL_SETTINGS_FILE, 'x') as file:
         settings_dict = settings.__dict__
         yaml.safe_dump(file, settings_dict)
 
 def load_local_settings() -> LocalSettings | None:
-    local_settings = None
     try:
-        with open(_local_settings_file, 'r') as file:
+        with open(_LOCAL_SETTINGS_FILE, 'r') as file:
             local_settings_yaml: dict = yaml.safe_load(file)
             bin_dirs = [Path(bin_dir) for bin_dir in local_settings_yaml['bin_dirs']]
 
-            local_settings = LocalSettings(
+            return LocalSettings(
                 bin_dirs=bin_dirs, 
                 output_dir=Path(local_settings_yaml['output_dir'])
             )
 
     except FileNotFoundError:
-        pass
-    return local_settings
+        return None
