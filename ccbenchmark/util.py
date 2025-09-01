84ae1ca946a192c101717d8b24ccdfa1c439736b
Dalton Prokosch <ddjjpp331@gmail.com>
2025-08-31 21:10:03 -0600

    added 'strip_common_paths to util'

--- ccbenchmark/util.py
+++ ccbenchmark/util.py
@@ -1,2 +1,24 @@
+from pathlib import Path
+from copy import deepcopy
+
 def time_to_str(time: float, unit: str) -> str:
     return f'{time:.2f} {unit}' if time is not None else 'N/A'
+
+def strip_common_paths(paths: list[Path]) -> list[Path]:
+    paths_copy = deepcopy(paths)
+    max_iterations = min([len(path.parts) for path in paths_copy])
+
+    for _ in range(max_iterations):
+        common_part = None
+        for path in paths_copy:
+            assert(len(path.parts) != 0)
+            part = path.parts[0]
+            if common_part is None:
+                common_part = part
+            elif common_part != part:
+                return paths_copy
+
+        for j in range(len(paths_copy)):
+            parts = paths_copy[j].parts[1:]
+            paths_copy[j] = Path(*parts)
+    raise Exception("Reached maximum path part depth!")  
