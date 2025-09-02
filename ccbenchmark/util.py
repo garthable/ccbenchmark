05908f2e657712107c1bda5b4c68ec4d368175ce
Dalton Prokosch <ddjjpp331@gmail.com>
2025-09-01 20:00:28 -0600

    fixed potential crash in strip_common_paths

--- ccbenchmark/util.py
+++ ccbenchmark/util.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 from copy import deepcopy
+import logging
+
+logger = logging.getLogger(__name__)
 
 def time_to_str(time: float, unit: str) -> str:
     return f'{time:.2f} {unit}' if time is not None else 'N/A'
 
 def strip_common_paths(paths: list[Path]) -> list[Path]:
     paths_copy = deepcopy(paths)
-    max_iterations = min([len(path.parts) for path in paths_copy])
+    lengths = [len(path.parts) for path in paths_copy]
+    if len(lengths) == 0:
+        logger.warning(f'No paths!')
+        return []
+    max_iterations = min(lengths)
 
     for _ in range(max_iterations):
         common_part = None
