fdccfd3759cdf96616d6b301ac14e5fd90219052
Dalton Prokosch <ddjjpp331@gmail.com>
2025-09-03 20:47:44 -0600

    modified how settings loads settings to take in multiple frameworks

--- ccbenchmark/benchmark_settings.py
+++ ccbenchmark/benchmark_settings.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 import yaml
+from typing import Any
 
 _LOCAL_SETTINGS_FILE = Path('./.ccbenchmark/settings.yaml')
 
@@ -14,24 +15,27 @@ class LocalSettings:
 def load_local_settings() -> LocalSettings | None:
     try:
         with open(_LOCAL_SETTINGS_FILE, 'r') as file:
-            local_settings_yaml: dict = yaml.safe_load(file)
+            local_settings_yaml: dict[str, dict] = yaml.safe_load(file)
+            local_settings = LocalSettings()
 
             default_benchmark_runnables = []
             default_output_dir = None
-            default_framework_name = None
             default_output_format = None
 
-            benchmark_runnables = [
-                Path(bin_dir) 
-                for bin_dir in local_settings_yaml.get('benchmark_runnables', default_benchmark_runnables)
-            ]
+            for framework_name, value in local_settings_yaml.items():
+                benchmark_runnables = [
+                    Path(bin_dir) 
+                    for bin_dir in value.get('benchmark_runnables', default_benchmark_runnables)
+                ]
+                output_dir = Path(value.get('output_dir', default_output_dir))
+                output_format = value.get('output_format', default_output_format)
 
-            return LocalSettings(
-                benchmark_runnables_list=[benchmark_runnables], 
-                output_dir_list=[Path(local_settings_yaml.get('output_dir', default_output_dir))],
-                framework_name_list=[local_settings_yaml.get('framework', default_framework_name)],
-                output_format_list=[local_settings_yaml.get('output_format', default_output_format)]
-            )
+                local_settings.benchmark_runnables_list.append(benchmark_runnables)
+                local_settings.output_dir_list.append(output_dir)
+                local_settings.framework_name_list.append(framework_name)
+                local_settings.output_format_list.append(output_format)
+
+        return local_settings
 
     except FileNotFoundError:
         return None
