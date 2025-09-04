e43902074daf5b391ea85f00a0df65a465317537
Dalton Prokosch <ddjjpp331@gmail.com>
2025-09-03 20:27:08 -0600

    modified settings to take in array of values

--- ccbenchmark/benchmark_settings.py
+++ ccbenchmark/benchmark_settings.py
@@ -6,22 +6,31 @@ _LOCAL_SETTINGS_FILE = Path('./.ccbenchmark/settings.yaml')
 
 @dataclass
 class LocalSettings:
-    benchmark_runnables: list[Path] = field(default_factory=lambda: [])
-    output_dir: Path = field(default_factory=lambda: Path())
-    framework: str = field(default_factory=lambda: '')
-    output_format: str = field(default_factory=lambda: '')
+    benchmark_runnables_list: list[list[Path]] = field(default_factory=lambda: [])
+    output_dir_list: list[Path] = field(default_factory=lambda: [])
+    framework_name_list: list[str] = field(default_factory=lambda: [])
+    output_format_list: list[str] = field(default_factory=lambda: [])
 
 def load_local_settings() -> LocalSettings | None:
     try:
         with open(_LOCAL_SETTINGS_FILE, 'r') as file:
             local_settings_yaml: dict = yaml.safe_load(file)
-            benchmark_runnables = [Path(bin_dir) for bin_dir in local_settings_yaml['benchmark_runnables']]
+
+            default_benchmark_runnables = []
+            default_output_dir = None
+            default_framework_name = None
+            default_output_format = None
+
+            benchmark_runnables = [
+                Path(bin_dir) 
+                for bin_dir in local_settings_yaml.get('benchmark_runnables', default_benchmark_runnables)
+            ]
 
             return LocalSettings(
-                benchmark_runnables=benchmark_runnables, 
-                output_dir=Path(local_settings_yaml['output_dir']),
-                framework=local_settings_yaml['framework'],
-                output_format=local_settings_yaml['output_format']
+                benchmark_runnables_list=[benchmark_runnables], 
+                output_dir_list=[Path(local_settings_yaml.get('output_dir', default_output_dir))],
+                framework_name_list=[local_settings_yaml.get('framework', default_framework_name)],
+                output_format_list=[local_settings_yaml.get('output_format', default_output_format)]
             )
 
     except FileNotFoundError:
