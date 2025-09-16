1a1e72edfc46c7a2b51c360a3c1f94258d86c633
Dalton Prokosch <ddjjpp331@gmail.com>
2025-09-16 15:51:34 -0600

    commented parse_result

--- src/ccbenchmark/frameworks/util/parse_result.py
+++ src/ccbenchmark/frameworks/util/parse_result.py
@@ -1,10 +1,21 @@
 from __future__ import annotations
+# Prevents circular imports
 import ccbenchmark.benchmark_data as bd
 from dataclasses import dataclass
-from typing import Any
 
 @dataclass(slots=True)
 class ParseResult:
+    """Result from parse framework function.
+    
+    Returned by a generator specifying the value of a cell within benchmark_data.
+
+    Attributes:
+        real_time: Time it takes based off a real clock.
+        cpu_time: Time it takes based off the cpu clock.
+
+        name: The name of the benchmark that was run.
+        metric_index: The column that this result belongs to.
+    """
     real_time: bd.BenchmarkTime
     cpu_time: bd.BenchmarkTime
 
